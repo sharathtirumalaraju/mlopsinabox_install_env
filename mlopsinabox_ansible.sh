a8d411165874c02c7d1d27443d7c004900d33dde
sharathtirumalaraju <tsharathvarma@gmail.com>
2021-05-12 11:58:30 +0200

    updated with python interpreter

--- mlopsinabox_ansible.sh
+++ mlopsinabox_ansible.sh
@@ -1,6 +1,6 @@
 sudo apt-get install python3 ansible
 echo "change the host file by deleting the default config and add localhost=127.0.0.1 and save it"
-sleep 7s
+sleep 5s
 gedit /etc/ansible/hosts
 sleep 30s
 ansible-playbook mlopsinabox.yml -e "ansible_python_interpreter=/usr/bin/python3"
