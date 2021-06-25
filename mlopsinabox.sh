4190d30ca69a7a457d8e7555768c009b13548940
sharathtirumalaraju <tsharathvarma@gmail.com>
2021-06-25 12:42:30 +0200

    updated according to the new changes in the cookie file

--- mlopsinabox.sh
+++ mlopsinabox.sh
@@ -12,9 +12,7 @@ git commit -m "Initialize DVC and git"
 read -p "enter your git repo uri:" gitrepo
 #add a git remote 
 git remote add origin $gitrepo
-read -p "user-email:" email
-read -p "user-name:" username
-python -c "from configparser import ConfigParser; c=ConfigParser(); c.read('config.ini'); c.set('git_repo','git_uri','$gitrepo'); c.set('git_repo','git_user_email','$email'); c.set('git_repo','git_user_name','$username'); f=open('config.ini','w'); c.write(f)"
+python -c "from configparser import ConfigParser; c=ConfigParser(); c.read('config.ini'); c.set('git_repo','git_uri','$gitrepo'); f=open('config.ini','w'); c.write(f)"
 #add a dvc remote storage using the path of storage directory
 dvc remote add -d localstorage $cwd/data/external
 #add dvc config to git
@@ -22,6 +20,6 @@ git add .dvc/config
 #this commit has info of the remote config with the git, which redirects it to the remote storage
 git commit -m "remote config"
 git push origin master
-mv config.ini $cwd/src/models
-mlflow server --backend-store-uri sqlite:///mlrun.db --default-artifact-root file:///mlrun.db
+
+
 
